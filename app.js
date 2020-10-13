802a1a08da8b8a5d44f8b0d7220e83553794523e
Nicolas VILLETTE <nicolas.j.l.agnus@gmail.com>
2020-10-13 18:14:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,20 @@ document.getElementById('clear_button').addEventListener("click", function (even
     return false;
 });
 
-console.log("11");
+function resizeCanvas() {
+    // When zoomed out to less than 100%, for some very strange reason,
+    // some browsers report devicePixelRatio as less than 1
+    // and only part of the canvas is cleared then.
+    var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+    canvas.width = canvas.offsetWidth * ratio;
+    canvas.height = canvas.offsetHeight * ratio;
+    canvas.getContext("2d").scale(ratio, ratio);
+}
+
+window.onresize = resizeCanvas;
+resizeCanvas();
+
+console.log("12");
 
 const {
   degrees,
